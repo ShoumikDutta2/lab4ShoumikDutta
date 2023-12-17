b6143344942408ecd70795709ed78612709d9a24
Shoumik Dutta <shoumikpranta@gmail.com>
2023-12-17 17:53:36 +0100

    Add project files.

---
+++ 16.1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.1416
+
+double distanceKm(double, double, double, double);
+double radianConversion(double);
+double overallDistance(double*, double*, int);
+
+int main()
+{
+	double lats[] = { 53.557029,53.557166,53.557274,53.560288,53.561306,53.562015,53.558241,53.557900,53.557203 };
+	double lons[] = { 10.022918,10.021343,10.020297,10.014906,10.015426,10.016568,10.023244,10.022142,10.022632 };
+
+	printf("Overall Distance: %lf", overallDistance(lats,lons,sizeof(lats)/sizeof(lats[0])));
+}
+
+double overallDistance(double *lats,double *lons, int arrsize) 
+{
+	double distance = 0.0;
+
+	for (int i = 0; i < arrsize-1; i++)
+	{
+		printf("%d %lf\n", i, lats[i]);
+		distance = distance + distanceKm(lats[i], lats[i + 1], lons[i], lons[i + 1]);
+
+	}
+
+	return distance;
+}
+
+double distanceKm(double lat1, double lat2, double lon1, double lon2)
+{
+	lat1 = radianConversion(lat1);
+	lon1 = radianConversion(lon1);
+	lat2 = radianConversion(lat2);
+	lon2 = radianConversion(lon2);
+
+	return 6378.388 * acos(sin((lat1)) * sin(lat2) + cos(lat1) * cos(lat2) * cos((lon2 - lon1)));
+}
+
+double radianConversion(double value)
+{
+	return value * (PI / 180.0);
+}
---
+++ 16.2.c
@@ -0,0 +1,60 @@
+﻿#include<stdio.h>
+#include<math.h>
+#define PI 3.1416
+double distanceKm(double , double , double , double);
+double radianConversion(double);
+double * longestDistance( double coordinates[7][2]);
+
+int main()
+{
+	double coordinates[7][2] = { {53.557078,10.023109}, {48.858363,2.294481},{39.562553,2.661947},{ 36.156214, -115.148736},{-22.971177 , -43.182543},{21.281004 , -157.837456},{-28.002695,153.431781} };
+	char *names[][100] = {"HAW Hamburg", "Eiffel Tower", "Palma de Mallorca", "Las Vegas", "Copacabana" , "Waikiki Beach", "Surfer's Paradise"};
+	double* result = longestDistance(coordinates);
+	printf("%s and %s have the largest distance %lf Km.\nYou'd better not walk. Take a flight, instead",names[0][(int)result[1]], names[0][(int)result[2]], result[0]);
+	
+
+	
+
+	return 0;
+}
+
+double * longestDistance(double coordinates[7][2])
+{
+	//i need aa 2d array where 0=distance,1=index for 1st place, 2= index of 2nd place
+	//sample {{distance,location1,location2}}
+	double result[3]; //this result will have the largest distance at 0th index ,2nd and 3rd index is for the index of coordinates
+	result[0] = 0.0;
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			double distance = distanceKm(coordinates[i][0], coordinates[j][0], coordinates[i][1], coordinates[j][1]);
+
+			if (distance > result[0])
+			{
+				result[0] = distance;
+				result[1] = i;
+				result[2] = j;
+			}
+		}
+
+
+	}
+
+	return result;
+
+}
+double distanceKm(double lat1, double lat2, double lon1, double lon2)
+{
+	lat1 = radianConversion(lat1);
+	lon1 = radianConversion(lon1);
+	lat2 = radianConversion(lat2);
+	lon2 = radianConversion(lon2);
+
+	return 6378.388 * acos(sin((lat1)) * sin(lat2) + cos(lat1) * cos(lat2) * cos((lon2 - lon1)));
+}
+
+double radianConversion(double value)
+{
+	return value * (PI / 180.0);
+}
